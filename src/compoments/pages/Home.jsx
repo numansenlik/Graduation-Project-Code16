16b5ade68eb19e6d209d29eebdad408d64dee854
Numan Şenlik <nmnsnlk@gmail.com>
2023-08-15 08:31:46 +0300

    form and comments

--- src/compoments/pages/Home.jsx
+++ src/compoments/pages/Home.jsx
@@ -55,25 +55,25 @@ function Home() {
             <Link to={"/products"}><button className="siteButton bannerButton">GET PRODUCTS</button></Link>
           </div>
         </div>
-        
- 
 
-              <div className="commentSection">
-                <div className="commentContainer">
-                  <img src="./src/images/open-quote.png" className="message-left" alt="message treatment"/>
-                  <p><strong>Luxurious experience, experienced stylists</strong></p>
-                  <p>I'm very pleased with my experience today at Beau Monde. Joy did a great job and I'm so happy I came. A beautiful and professional salon with top of the line customer service. I will definetly be back.</p>
-                  <img src="./src/images/close-quote.png" className="message-right" alt="message treatment"/>
-                  <span className="commentCustomerName">- MAY PADDINGTON</span>
-                </div>
 
-                <div className="commentTextArea">
-                  <h3>Make an Appointment</h3>
-                  <br/>
-                  <p>All of our service providers at Beau Monde Salon and Spa are cotinuosly greatful for the business and support of our wonderful guests. Please reserve an appointment with any of our team members as all of our stylists schedules fill quickly. We do occasionally have last-minute openings, but appointments are recommended.</p>
-                  <button className="siteButton bannerButton">MAKE APPOINTMENT</button>
-                </div>  
-              </div>
+
+        <div className="commentSection">
+          <div className="commentContainer">
+            <img src="./src/images/open-quote.png" className="message-left" alt="message treatment" />
+            <p><strong>Luxurious experience, experienced stylists</strong></p>
+            <p>I'm very pleased with my experience today at Beau Monde. Joy did a great job and I'm so happy I came. A beautiful and professional salon with top of the line customer service. I will definetly be back.</p>
+            <img src="./src/images/close-quote.png" className="message-right" alt="message treatment" />
+            <span className="commentCustomerName">- MAY PADDINGTON</span>
+          </div>
+
+          <div className="commentTextArea">
+            <h3>Make an Appointment</h3>
+            <br />
+            <p>All of our service providers at Beau Monde Salon and Spa are cotinuosly greatful for the business and support of our wonderful guests. Please reserve an appointment with any of our team members as all of our stylists schedules fill quickly. We do occasionally have last-minute openings, but appointments are recommended.</p>
+            <button className="siteButton bannerButton">MAKE APPOINTMENT</button>
+          </div>
+        </div>
 
       </div>
     </>
