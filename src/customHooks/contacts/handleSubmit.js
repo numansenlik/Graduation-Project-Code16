1f7eb02d6c29ee0de2ee19e6254ca9aefb4a1d6a
Numan Şenlik <nmnsnlk@gmail.com>
2023-08-29 15:41:55 +0300

    add send email

--- src/customHooks/contacts/handleSubmit.js
+++ src/customHooks/contacts/handleSubmit.js
@@ -1,19 +1,7 @@
-import contactsGetApi from "./contactsGetApi";
 import contacsPutApi from "./contacsPutApi";
 
-const handleSubmit = async (e,data) => {
-  e.preventDefault();
-
-  const email = e.target[0].value;
-  const firstName = e.target[1].value;
-  const lastName = e.target[2].value;
-  const title = e.target[3].value;
-  const commentText = e.target[4].value;
-
-  if (!email || !firstName || !lastName || !title || !commentText) {
-    console.log("Form fields cannot be empty. Submit canceled.");
-    return;
-  }
+const handleSubmit = async (values, data) => {
+  const { email, firstName, lastName, title, comment } = values;
 
   const newData = [
     ...data,
@@ -23,14 +11,13 @@ const handleSubmit = async (e,data) => {
       email: email,
       comment: {
         title: title,
-        text: commentText,
+        text: comment, 
       },
     },
   ];
 
-  e.target.reset();
   contacsPutApi(newData);
-  return newData
+  return newData;
 };
 
-export default handleSubmit
+export default handleSubmit;
